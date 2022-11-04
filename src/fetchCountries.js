3fb32becbc3f37e0ef8cf8e17bfa14fe1cc83162
MsMirzakhanova <98583249+MsMirzakhanova@users.noreply.github.com>
2022-11-04 17:22:47 +0200

    processing

---
+++ src/fetchCountries.js
@@ -0,0 +1,12 @@
+import Notiflix from "notiflix";
+
+export default function fetchCountries(name) {
+return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`).then(response => response.json());
+}
+
+//.then if response.length > 10 {
+//Notiflix.Notify("Too many matches found. Please enter a more specific name.")
+//}).then reurn fetch((`https://restcountries.com/v3.1/name/{name}`))
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,60 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
-
-var debounce = require('lodash.debounce');
+import fetchCountries from "./fetchCountries"
+//import debounce from 'lodash/debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+
+searchBox = document.querySelector(`input#search-box`)
+console.log(searchBox);
+
+searchBox.addEventListener(
+    "input", (event) => {
+        const inputValue = event.target.value.trim();
+        
+        if (inputValue) handleInput(inputValue);
+        
+});
+    
+function handleInput (value) {
+            setTimeout(() => {
+                fetchCountries(value)
+                    .then(countries => {
+                if (countries.length > 10) {
+                    Notiflix.Notify.failure(`Too many matches found. Please enter a more specific name.`);
+                }
+                else if (countries.length <= 10 && countries.length >= 2) {
+                    displayCountriesInfo(countries)
+                } else {
+                    displayCountryInfo(countries[0])
+                    }
+                    }
+                )
+                    .catch(error => {
+                      Notiflix.Notify.error(`Oops, there is no country with that name.`);  
+                    }) 
+        }, DEBOUNCE_DELAY);
+}
+
+function displayCountryInfo(country) {
+
+ console.log(`country`,country);   
+};
+
+function displayCountriesInfo(countries) {
+    console.log(`countries`, countries); 
+    const countryList = document.querySelector(`.country-list`);
+    countries.forEach(country => {
+        const li = document.createElement(`li`)  
+        const span = document.createElement(`span`)
+        span.innerHTML = country.name.official;
+        const img = document.createElement(`img`)
+        img.src = country.flags.svg
+        
+        li.append(span, img);
+        //console.log(li);
+        countryList.append(li);
+
+    });
+};
